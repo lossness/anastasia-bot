f42f79521a5cb2c278dac73da5bf87d28cd81b55
Joel Korcsmaros <20404254+lossness@users.noreply.github.com>
2020-01-20 18:07:40 -0800

    added env passwords

--- anastasia/sms_on_mention.py
+++ anastasia/sms_on_mention.py
@@ -1,21 +1,27 @@
-import smtplib, os, textwrap
+import smtplib
+import os
+
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
-from data import sms_info 
 
+
+
+load_dotenv()
+SMS_EMAIL = os.getenv("SMS_EMAIL")
+SMS_PASS = os.getenv("SMS_PASS")
 
 def send_text(phone, carrier_gateway, body):
-    email = "anastasia.bot.01@gmail.com"
-    pas = "eyeamarobottrick"
+    email = SMS_EMAIL
+    pas = SMS_PASS
     # email = os.getenv("SMS_EMAIL")
     # pas = os.getenv("SMS_PASS")
     smtp = "smtp.gmail.com"
     port = 587
     sms_gateway = "{}{}".format(phone, carrier_gateway)
-    server = smtplib.SMTP(smtp,port)
+    server = smtplib.SMTP(smtp, port)
     server.starttls()
-    server.login(email,pas)
+    server.login(email, pas)
     # now we use the MIME module to structure our message.
     msg = MIMEMultipart()
     msg['From'] = email
@@ -27,8 +33,7 @@ def send_text(phone, carrier_gateway, body):
     # and then attach that body furthermore you can also send html content
     msg.attach(MIMEText(wrapped_body, 'plain'))
     sms = msg.as_string()
-    server.sendmail(email,sms_gateway,sms)
+    server.sendmail(email, sms_gateway, sms)
     # quit the server
     server.quit()
 
-send_text("8177890010", "@tmomail.net", "Anastasia has booted up.")
